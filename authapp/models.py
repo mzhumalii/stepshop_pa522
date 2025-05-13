e49117aff9a78c1b306a29d56463f2cbd0f5a795
mzhumalii <miraszhumali2008@gmail.com>
2025-05-13 18:43:05 +0500

    lesson_07: ShopUser

---
+++ authapp/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class ShopUser(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='avatars',
+        blank=True,
+    )
+
+    age = models.PositiveIntegerField(
+        verbose_name='Возраст',
+        default=0,
+    )
