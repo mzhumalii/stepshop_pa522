78fa0ab24349d6b91d5fe01226ba688f9db3ef6d
mzhumalii <miraszhumali2008@gmail.com>
2025-05-15 18:29:41 +0500

    lesson_08: forms

--- stepshop/urls.py
+++ stepshop/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='mainapp')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
+
 
 
 if settings.DEBUG:
