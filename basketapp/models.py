3b5ae71365de3337bb2bfdd8d431dc2267d421ab
mzhumalii <miraszhumali2008@gmail.com>
2025-06-10 18:40:37 +0500

    lesson_13: basket

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from mainapp.models import Product
+from stepshop import settings
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='basket',
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество',
+        default=0,
+    )
+
+    add_datetime = models.DateTimeField(
+        verbose_name='Время',
+        auto_now_add=True,
+    )
